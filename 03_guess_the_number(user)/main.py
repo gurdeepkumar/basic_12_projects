bf1dbda360a408ee1d2ca123e2d3975c1e7c1522
Gurdeep kumar <gurdeepkumar1993@hotmail.com>
2023-12-20 22:46:20 +0000

    Third project added.

---
+++ 03_guess_the_number(user)/main.py
@@ -0,0 +1,33 @@
+#import the random lib
+import random
+
+#define a function
+def computer_guess(x):
+  feedback = " "
+  
+  #assign variable of range 
+  low = 1
+  high = x
+
+  while feedback != "c":
+    #let computer have a random guess
+    guess = random.randint(low, high)
+    print(f"The guess is: {guess}")
+
+    #check if the guess is too low, too high or correct
+    feedback = input("Enter Correct(C), too low(L) or too high(H): ").lower()
+    if feedback == "h":
+      high = guess - 1
+    elif feedback == "l":
+      low = guess + 1
+  
+  print(f"The computer guessed your number {guess}.")
+
+print("Pick a number between 1 and 100. In this game, computer is going to guess the number. Please provide feedback using letters to play the game")
+
+start = input("Are you ready ? Press yes(Y) or no(N):  ").lower()
+
+if start == "y":
+  computer_guess(100)
+elif start == "n":
+  print("You entered no and the game is quiting.")
